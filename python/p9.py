57459d33d0e2c3e97e0a569e7054815c03cb48a9
Hacksmith01 <ishanpatil2005@gmail.com>
2024-08-21 14:29:36 +0530

    updated solution

--- python/p9.py
+++ python/p9.py
@@ -4,5 +4,6 @@ number = int(input("Enter the number you want to find the table of: "))
 rang=int(input("Enter the range till where you want to find the multiples: "))
 
 for i in range(rang+1):
-    print(f"{number} x" ,i,"==",number*i)
+    #print(f"{number} x" ,i,"=",number*i)
     i=i+1
+    print('%2s * %2s = %2s'%(number,i,number*i))
