0ecae38be95b3c13ce3e5a3276d13e10e74d83d3
Hacksmith01 <ishanpatil2005@gmail.com>
2024-08-21 15:35:36 +0530

    solved p11

--- python/p11.py
+++ python/p11.py
@@ -1,10 +1,10 @@
 # Program to Find Sum of digits of a number
 
-number=int(input("Enter the number you want to find the sum of digits: "))
+number=input("Enter the number you want to find the sum of digits: ")
 summ=0
 for i in number:
-    summ=summ+i
-    i=i+1
+    x=int(i)
+    summ=summ+x
 print(summ)
 
 
