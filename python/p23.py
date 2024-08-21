5d591898c90c6f4607b6322271a74cbd783a5545
Hacksmith01 <ishanpatil2005@gmail.com>
2024-08-21 17:26:10 +0530

    added p23

---
+++ python/p23.py
@@ -0,0 +1,12 @@
+# Program to Print X shape inside Hollow Square
+
+lines=int(input("Enter the number of lines you want to print X: "))
+
+for i in range(1,lines+1):
+    for j in range(1,lines+1):
+        if j == 1 or j == lines or i == j or j == lines - i + 1:
+            print("* ", end='')
+        else:
+            print("  ",end='')
+    print()
+
