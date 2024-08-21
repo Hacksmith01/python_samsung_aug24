75b5f2caf43dd7b6d2fb5a6b6601337966d4140f
Hacksmith01 <140841051+Hacksmith01@users.noreply.github.com>
2024-08-21 21:54:20 +0530

    Update p20.py

--- python/p20.py
+++ python/p20.py
@@ -6,8 +6,11 @@ Program to print the Equi Lateral Triangle of N lines
  *******
 *********
 '''
-number = int(input("Enter the number you want to display the Equi Lateral Triangle of N lines: "))
-for i in range(number +1 ):
+number = int(input("Enter the number of lines for the Equilateral Triangle: "))
+
+for i in range(number):
+    print(' ' * (number - i - 1), end='')
+    print('*' * (2 * i + 1))
 
 
 
