6e7c0d73809b1bcd62ade6854fe7d50d3592f99a
Hacksmith01 <ishanpatil2005@gmail.com>
2024-08-28 16:53:18 +0530

    solve mini_prj

---
+++ python/database_programs/mini_prj.py
@@ -0,0 +1,19 @@
+# write a python program to compile a python program
+''' take a 4 digit number in which the digits can repeat 2 times only
+rearrange the digits in decreasing order 
+
+take a 4 digit number in which the digits can repeat 2 times only
+rearrange the digits in increasing order 
+
+take the difference, check if the difference number is same as the previous diffrence
+if yes print the count of the number of time the algorithm applied 
+else repeat the algorithm with the difference number as the original number '''
+
+
+
+num=int(input("Enter the number:" ))
+
+
+
+
+
